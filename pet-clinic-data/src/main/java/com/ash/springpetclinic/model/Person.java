cbc6bd4ba877a9bbb9738dc1ab098dcb1f4cafec
iamashutosh <akashutosh100@gmail.com>
2020-10-31 16:17:05 +0530

    addiding Base entity. Closes #10

--- pet-clinic-data/src/main/java/com/ash/springpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/ash/springpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.ash.springpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
     private String firstname;
     private String lastname;
 
--- pet-clinic-data/src/main/java/com/ash/springpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/ash/springpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.ash.springpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private  String name;
 
